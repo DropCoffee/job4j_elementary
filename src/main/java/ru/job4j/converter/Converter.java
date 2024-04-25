fcf9ec87fa94901ea65e3913a52317454c507462
DropCoffee <malyukov.maksim90@yandex.ru>
2024-04-25 23:45:28 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -14,5 +14,17 @@ public class Converter {
         System.out.println("140 rubles are " + euro + " euro");
         float dollar = Converter.rubleToDollar(60);
         System.out.println("60 rubles are " + dollar + " dollar");
+
+        float inputRubToEuro = 140;
+        float exceptedEuro = 2;
+        float outputEuro = Converter.rubleToEuro(inputRubToEuro);
+        boolean passedEuro = exceptedEuro == outputEuro;
+        System.out.println("140 rubles are 2. Test result: " + passedEuro);
+
+        float inputRubToDollar = 60;
+        float exceptedDollar = 1;
+        float outputDollar = Converter.rubleToDollar(inputRubToDollar);
+        boolean passedDollar = exceptedDollar == outputDollar;
+        System.out.println("60 rubles are 1. Test result: " + passedDollar);
     }
 }
