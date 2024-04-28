84d890b8934d892b38f007f2843200d5f57c94bd
DropCoffee <malyukov.maksim90@yandex.ru>
2024-04-28 13:19:14 +0300

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/calculator/MathFunction.java
+++ src/main/java/ru/job4j/calculator/MathFunction.java
@@ -2,6 +2,14 @@ package ru.job4j.calculator;
 
 public class MathFunction {
 
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
     public static int func1(int x) {
         return x * x + 1;
     }
